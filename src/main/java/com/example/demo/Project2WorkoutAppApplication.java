539c152056e2410454245935dbdd3a930a4a9762
MAThatcher <mitchellathatcher96@gmail.com>
2021-02-09 15:31:41 -0800

    Init
    Init

---
+++ src/main/java/com/example/demo/Project2WorkoutAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.revature")
+@EnableJpaRepositories("com.revature.repositories")
+@EntityScan("com.revature.beans")
+public class Project2WorkoutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2WorkoutAppApplication.class, args);
+	}
+
+}

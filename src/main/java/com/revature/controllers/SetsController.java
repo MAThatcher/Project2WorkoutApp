46115eff410667e6a720a9855318470603918d50
camtheconqueror <weaver.cameron@gmail.com>
2021-02-10 16:18:12 -0600

    Sets created
    Repo/Service/Controller created for sets

--- src/main/java/com/revature/controllers/SetsController.java
+++ src/main/java/com/revature/controllers/SetsController.java
@@ -1,5 +1,21 @@
 package com.revature.controllers;
 
-public class SetsController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.revature.beans.Sets;
+
+public interface SetsController {
+
+	public Sets getSet(@PathVariable("id") String id);
+	public List<Sets> getAllSets();
+	public Sets addSet(@RequestBody Sets set);
+	public Sets updateSet(@PathVariable("id") int id, @RequestBody Sets change);
+	public boolean deleteSet(@PathVariable("id") int id);
 }

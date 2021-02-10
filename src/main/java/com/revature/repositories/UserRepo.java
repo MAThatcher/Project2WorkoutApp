8761cef21d58cc0353945afc2907f94688764a8b
CalebKnittle <calebmk9@vt.edu>
2021-02-10 17:05:29 -0500

    Added User Repos, Services, Controller

---
+++ src/main/java/com/revature/repositories/UserRepo.java
@@ -0,0 +1,16 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.beans.User;
+
+
+@Repository
+public interface UserRepo extends CrudRepository <User, Integer>{
+	
+	List<User> findByUsername(String username);
+
+}

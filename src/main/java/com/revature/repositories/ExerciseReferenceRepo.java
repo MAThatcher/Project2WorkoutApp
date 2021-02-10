f0a2afaedbf6580177a5debd10386b249d27b88d
MAThatcher <mitchellathatcher96@gmail.com>
2021-02-10 14:33:31 -0800

    Update ExerciseReferenceRepo.java

--- src/main/java/com/revature/repositories/ExerciseReferenceRepo.java
+++ src/main/java/com/revature/repositories/ExerciseReferenceRepo.java
@@ -1,8 +1,10 @@
 package com.revature.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.beans.ExerciseReference;
 
+@Repository
 public interface ExerciseReferenceRepo extends CrudRepository<ExerciseReference, Integer>{
 }

d11da1c3ad39120a6a4003d405e23282264dd169
NickUngar1 <76971050+NickUngar1@users.noreply.github.com>
2021-02-10 17:29:59 -0500

    Update WorkoutServiceImpl.java

--- src/main/java/com/revature/services/WorkoutServiceImpl.java
+++ src/main/java/com/revature/services/WorkoutServiceImpl.java
@@ -3,10 +3,12 @@ package com.revature.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.beans.Workout;
 import com.revature.repositories.WorkoutRepo;
 
+@Service
 public class WorkoutServiceImpl implements WorkoutService {
 	
 	@Autowired

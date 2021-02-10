3ca437ce39469f249add9959630cd88efb92e92d
camtheconqueror <weaver.cameron@gmail.com>
2021-02-10 17:55:42 -0600

    wtf
    wtf

--- src/main/java/com/revature/services/WorkoutService.java
+++ src/main/java/com/revature/services/WorkoutService.java
@@ -8,6 +8,7 @@ public interface WorkoutService {
 
 	public Workout addWorkout(Workout w);
 	public Workout getWorkout(int id);
+	public Workout getWorkout(String name);
 	public List<Workout> getAllWorkouts();
 	public Workout updateWorkout(Workout change);
 	public boolean deleteWorkout(int id);

8aa3517a569c0f808686e47058d18ffe7f43f87a
camtheconqueror <weaver.cameron@gmail.com>
2021-02-10 16:30:39 -0600

    Update Sets.java
    updated foreign keys

--- src/main/java/com/revature/beans/Sets.java
+++ src/main/java/com/revature/beans/Sets.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -16,66 +18,83 @@ public class Sets {
 	@Column(updatable = false)
 	@SequenceGenerator(name = "SETS_SEQ", sequenceName = "SETS_SEQ", allocationSize = 1)
 	@GeneratedValue(generator = "SETS_SEQ", strategy = GenerationType.SEQUENCE)
-	int set_id;
+	private int set_id;
 	
 	@Column(name = "repetitions")
-	int repetitions;
+	private int repetitions;
 	
 	@Column(name = "total_calories")
-	int total_calories;
+	private int total_calories;
 	
-	@Column(name = "ex_id")
-	int ex_id;
-	
-	@Column(name = "workout_id")
-	int workout_id;
-	
+	@ManyToOne
+	@JoinColumn(name = "ex_id")
+	private ExerciseReference exercise;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_id")
+	private Workout workout;
+
 	public Sets() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Sets(int set_id, int repetitions, int total_calories, int ex_id, int workout_id) {
+
+	public Sets(int set_id, int repetitions, int total_calories, ExerciseReference exercise, Workout workout) {
 		super();
 		this.set_id = set_id;
 		this.repetitions = repetitions;
 		this.total_calories = total_calories;
-		this.ex_id = ex_id;
-		this.workout_id = workout_id;
+		this.exercise = exercise;
+		this.workout = workout;
 	}
+
 	public int getSet_id() {
 		return set_id;
 	}
+
 	public void setSet_id(int set_id) {
 		this.set_id = set_id;
 	}
+
 	public int getRepetitions() {
 		return repetitions;
 	}
+
 	public void setRepetitions(int repetitions) {
 		this.repetitions = repetitions;
 	}
+
 	public int getTotal_calories() {
 		return total_calories;
 	}
+
 	public void setTotal_calories(int total_calories) {
 		this.total_calories = total_calories;
 	}
-	public int getEx_id() {
-		return ex_id;
+
+	public ExerciseReference getExercise() {
+		return exercise;
 	}
-	public void setEx_id(int ex_id) {
-		this.ex_id = ex_id;
+
+	public void setExercise(ExerciseReference exercise) {
+		this.exercise = exercise;
 	}
-	public int getWorkout_id() {
-		return workout_id;
+
+	public Workout getWorkout() {
+		return workout;
 	}
-	public void setWorkout_id(int workout_id) {
-		this.workout_id = workout_id;
+
+	public void setWorkout(Workout workout) {
+		this.workout = workout;
 	}
+
 	@Override
 	public String toString() {
-		return "Sets [set_id=" + set_id + ", repetitions=" + repetitions + ", total_calories=" + total_calories
-				+ ", ex_id=" + ex_id + ", workout_id=" + workout_id + "]";
+		return "Sets [set_id=" + set_id + ", repetitions=" + repetitions + ", total_calories=" + total_calories + "]";
 	}
+
+    
+	
+
 	
 }

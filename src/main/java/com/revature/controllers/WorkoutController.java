7b6265d2855258daff36555f60c03e91600d11ab
MAThatcher <mitchellathatcher96@gmail.com>
2021-02-17 10:05:00 -0800

    .
    .

--- src/main/java/com/revature/controllers/WorkoutController.java
+++ src/main/java/com/revature/controllers/WorkoutController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.revature.beans.Workout;
 import com.revature.services.WorkoutServiceImpl;
 
 @RestController
+@CrossOrigin
 public class WorkoutController {
 	
 	@Autowired
